0078127c4653dd02560d198fe3aeafd977b5ae21
pranayrawatresec <pranay@resecsystems.io>
2024-10-07 11:02:17 +0530

    about us page

--- js/testimonial-slider.js
+++ js/testimonial-slider.js
@@ -28,6 +28,24 @@ setInterval(nextSlide, 3000); // Automatically change slides every 3 seconds
 showSlide(currentSlideIndex);
 
 
+const scrollRevealOption={
+    distance:'90px',
+    origin:'bottom',
+    duration:1200
+}
+ScrollReveal().reveal('.benefit-left h2, .benefit-left h1',{...scrollRevealOption,delay:'100'})
+ScrollReveal().reveal(".benefit-head-desc",{...scrollRevealOption,origin:'right',delay:'200'})
+ScrollReveal().reveal(".benefit-left-points",{...scrollRevealOption,origin:'right',delay:'300'})
+
+ScrollReveal().reveal('.why-carmaa-container h1, .why-carmaa-container h2,.why-carmaa-container .header-description',{...scrollRevealOption,delay:'100'})
+ScrollReveal().reveal('.why-carmaa-container .header-description',{...scrollRevealOption,delay:'200'})
+ScrollReveal().reveal('.why-carmaa-card-container',{...scrollRevealOption,origin:'right',distance:'200px', duration:1500,delay:'300'})
+
+ScrollReveal().reveal('.benefit-element-wrapper-container',{...scrollRevealOption,origin:'left',distance:'200px', duration:1500,delay:'200'})
+
+ScrollReveal().reveal('.parallex-cta-inner',{...scrollRevealOption,distance:'100px', duration:1500,delay:'100'});
+
+
 
 
 
