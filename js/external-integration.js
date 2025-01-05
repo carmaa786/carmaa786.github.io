8e6bf49cb77aa6a443b5c3810f3760f89f2134f8
ankit <ap384069@gmail.com>
2025-01-05 15:04:39 +0530

    Revert " Neo Dove API Issue Resolved,"
    This reverts commit 2cc688306513b1df81bb1614cc0a3485d8152212.

--- js/external-integration.js
+++ js/external-integration.js
@@ -1,40 +1,40 @@
 export const CTAApi = async (data) => {
-  const response1 = await fetch("https://cc82c132-219a-4fb6-80ba-8b40d2f70f0f.neodove.com/integration/custom/72e4358b-fa93-4983-a1d4-cea3d4340d97/leads", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  const response = await fetch('https://sxmcshw4rhnmioduf75cgpzw6i0fzlxt.lambda-url.ap-south-1.on.aws/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  return response;
-};
+ const response1=await fetch("https://cc82c132-219a-4fb6-80ba-8b40d2f70f0f.neodove.com/integration/custom/72e4358b-fa93-4983-a1d4-cea3d4340d97/leads",{
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+    const response = await fetch('https://sxmcshw4rhnmioduf75cgpzw6i0fzlxt.lambda-url.ap-south-1.on.aws/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return response;
+  };
 
 
 
+  
 
-
-export const franchiseApi = async (data) => {
-  const response1 = await fetch("https://cc82c132-219a-4fb6-80ba-8b40d2f70f0f.neodove.com/integration/custom/34f3e4b7-7463-4047-8ef2-58fe8e7fccb8/leads", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  const response = await fetch('https://sxmcshw4rhnmioduf75cgpzw6i0fzlxt.lambda-url.ap-south-1.on.aws/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  return response;
-};
-
+  export const franchiseApi = async (data) => {
+    const response1=await fetch("https://cc82c132-219a-4fb6-80ba-8b40d2f70f0f.neodove.com/integration/custom/34f3e4b7-7463-4047-8ef2-58fe8e7fccb8/leads",{
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(data),
+   })
+       const response = await fetch('https://sxmcshw4rhnmioduf75cgpzw6i0fzlxt.lambda-url.ap-south-1.on.aws/', {
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json',
+         },
+         body: JSON.stringify(data),
+       });
+       return response;
+     };
+   
