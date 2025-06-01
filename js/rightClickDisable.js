e55c26f4b8d9845c5f57e879a430067964ba5d7d
MEGHA JANGWAN <jangwanmegha@gmail.com>
2025-06-01 16:25:25 +0530

    homepage services images resize

--- js/rightClickDisable.js
+++ js/rightClickDisable.js
@@ -1,21 +1,21 @@
-// document.addEventListener(
-//   "contextmenu",
-//   function (e) {
-//     e.preventDefault();
-//   },
-//   false
-// );
+document.addEventListener(
+  "contextmenu",
+  function (e) {
+    e.preventDefault();
+  },
+  false
+);
 
-// // Disable Specific Keyboard Shortcuts
-// document.addEventListener("keydown", function (event) {
-//   // Disable Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U (View Source), and F12 (DevTools)
-//   if (
-//     (event.ctrlKey &&
-//       event.shiftKey &&
-//       (event.key === "I" || event.key === "J")) ||
-//     (event.ctrlKey && event.key === "U") ||
-//     event.key === "F12"
-//   ) {
-//     event.preventDefault();
-//   }
-// });
+// Disable Specific Keyboard Shortcuts
+document.addEventListener("keydown", function (event) {
+  // Disable Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U (View Source), and F12 (DevTools)
+  if (
+    (event.ctrlKey &&
+      event.shiftKey &&
+      (event.key === "I" || event.key === "J")) ||
+    (event.ctrlKey && event.key === "U") ||
+    event.key === "F12"
+  ) {
+    event.preventDefault();
+  }
+});
