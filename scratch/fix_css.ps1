218ba86fd15da4297849310f35fe7355a5c59bf6
Kiran Joshi <kiranjoshi1649@gmail.com>
2026-05-13 14:38:08 +0530

    chore: clean up unused assets and temporary folders

--- scratch/fix_css.ps1
+++
@@ -1,5 +0,0 @@
-$content = Get-Content css\city.css -Raw
-$pattern = '(?s)max-width: 500px;.*?margin: 0 auto;.*?display: block;.*?\}'
-$replacement = 'max-width: 500px;`r`n        margin: 0 auto;`r`n        display: block;`r`n    }`r`n}'
-$newContent = $content -replace $pattern, $replacement
-Set-Content css\city.css $newContent -NoNewline
